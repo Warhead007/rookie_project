f0c8f589259848ad8fbf86fadf5509aa3902cf6c
Warhead007 <rawit_ham@hotmail.com>
2019-07-04 15:02:34 +0700

    Add database connection and add function get user data

--- Rookie/main.go
+++ Rookie/main.go
@@ -8,18 +8,34 @@ import (
 	"strconv"
 	"time"
 
+	"gopkg.in/mgo.v2"
+
 	"github.com/labstack/echo"
+	"gopkg.in/mgo.v2/bson"
 )
 
-type userData struct {
-	Name   string `json:"name"`
-	Avatar string `json:"avatar"`
-	Age    int    `json:"age"`
-	Note   string `json:"note"`
-	Email  string `json:"email"`
+const (
+	server     = "localhost:27017"
+	database   = "rookie_trainer"
+	collection = "user_data"
+)
+
+//UserData struct//
+type UserData struct {
+	ID          bson.ObjectId `bson:"_id"`
+	Name        string        `bson:"name" json:"name"`
+	Avatarname  string        `bson:"avatar_name" json:"avatar_name"`
+	Avatartype  string        `bson:"avatar_type" json:"avatar_type"`
+	Age         int           `bson:"age" json:"age"`
+	Yearofbirth int           `bson:"year_of_birth" json:"year_of_birth"`
+	Note        string        `bson:"note,omitempty" json:"note,omitempty"`
+	Email       string        `bson:"email" json:"email"`
+	Createtime  string        `bson:"create_time" json:"create_time"`
+	Updatetime  string        `bson:"update_time" json:"update_time"`
 }
 
-type errorMessage struct {
+//ErrorMessage struct//
+type ErrorMessage struct {
 	Code        string `json:"code"`
 	Description string `json:"description"`
 }
@@ -28,25 +44,18 @@ func main() {
 
 	e := echo.New()
 	e.POST("/user", createUser)
-	e.POST("/save", getData)
+	e.POST("/save", addData)
 
 	e.Logger.Fatal(e.Start(":1323"))
 }
 
 func createUser(c echo.Context) error {
-	u := new(userData)
-	if err := c.Bind(u); err != nil {
-		return err
-	}
-	///check require data///
-	if u.Age == 0 || u.Name == "" || u.Email == "" || u.Avatar == "" {
-		return c.String(http.StatusBadRequest, "Plase enter data")
-	}
-	return c.JSON(http.StatusCreated, u)
+
+	return c.JSON(http.StatusCreated, addData(c))
 }
 
 ///function to get data from user///
-func getData(c echo.Context) error {
+func addData(c echo.Context) error {
 	name := c.FormValue("name")
 	age := c.FormValue("age")
 	email := c.FormValue("email")
@@ -55,10 +64,19 @@ func getData(c echo.Context) error {
 	if err != nil {
 		return err
 	}
+	///open session to connect database///
+	session, err := mgo.Dial(server)
+	if err != nil {
+		return err
+	}
+	defer session.Close()
+
+	///access to database and collection to using data///
+	a := session.DB(database).C(collection)
 	//check email format//
 	re := regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
 	///email invalid error message in JSON///
-	emailError := &errorMessage{
+	emailError := &ErrorMessage{
 		Code:        "401",
 		Description: "Invalid email format",
 	}
@@ -106,7 +124,7 @@ func getData(c echo.Context) error {
 		return c.HTML(http.StatusBadRequest, "")
 	}
 	///file type error message in JSON///
-	fileError := &errorMessage{
+	fileError := &ErrorMessage{
 		Code:        "401",
 		Description: "Invalid file type. Upload .png or .jpg/.jpeg only",
 	}
@@ -122,7 +140,7 @@ func getData(c echo.Context) error {
 		return err
 	}
 	///age error message in JSON///
-	ageError := &errorMessage{
+	ageError := &ErrorMessage{
 		Code:        "401",
 		Description: "Invalid age. You age must in range of 1 - 100",
 	}
@@ -134,11 +152,62 @@ func getData(c echo.Context) error {
 	createTime := time.Format("15:04:05 02-01-2006")
 	updateTime := createTime
 
-	return c.HTML(http.StatusOK, " "+name+" "+age+" "+strconv.Itoa(yearOfBirth)+" "+email+" "+note+" "+contentType+" "+createTime+" "+updateTime+" ")
+	///get data from user store into JSON format///
+	add := &UserData{
+		ID:          bson.NewObjectId(),
+		Name:        name,
+		Avatarname:  avatar.Filename,
+		Avatartype:  contentType,
+		Age:         conAge,
+		Yearofbirth: yearOfBirth,
+		Note:        note,
+		Email:       email,
+		Createtime:  createTime,
+		Updatetime:  updateTime,
+	}
+	///check error when email exists///
+	emailExists := &ErrorMessage{
+		Code:        "401",
+		Description: "Email is already used. Plase change email",
+	}
+	///check email with count a found data in database///
+	count, err := a.Find(bson.M{"email": &add.Email}).Count()
+	if err != nil {
+		return err
+	}
+	///if found count != 0///
+	if count > 0 {
+		return c.JSON(http.StatusUnauthorized, emailExists)
+	}
+	///can add data to database///
+	a.Insert(add)
+	return c.JSON(http.StatusCreated, getUserData(add.ID))
+}
+
+///function get one user by ID///
+func getUserData(id bson.ObjectId) UserData {
+	///open session to connect database///
+	session, err := mgo.Dial(server)
+	if err != nil {
+		panic(err)
+	}
+	defer session.Close()
+
+	///access to database and collection to using data///
+	a := session.DB(database).C(collection)
+	userdata := UserData{}
+	///find ID of user///
+	a.Find(bson.M{"_id": id}).One(&userdata)
+	if err != nil {
+		panic(err)
+	}
+	///return in JSON format///
+	return userdata
 }
 
 ///function to get file type///
 func getFileType(out *os.File) (string, error) {
+	///read file in first 512 byte to check file type///
 	buffer := make([]byte, 512)
 
 	_, err := out.Read(buffer)
