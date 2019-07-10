c7553434ec452fe5a92310e3d37c774924828b88
Warhead007 <rawit_ham@hotmail.com>
2019-07-10 17:40:04 +0700

    Fix comment format

--- Rookie/pkg/controller.go
+++ Rookie/pkg/controller.go
@@ -11,7 +11,7 @@ import (
 	"gopkg.in/mgo.v2/bson"
 )
 
-//ErrorMessage to store error message in JSON//
+//ErrorMessage to store error message in JSON
 type ErrorMessage struct {
 	Code        string `json:"code"`
 	Description string `json:"description"`
@@ -22,7 +22,7 @@ var internalError = &ErrorMessage{
 	Description: "Internal Error. Plase try again.",
 }
 
-//AddUser :function to get data from user//
+//AddUser :function to get data from user
 func AddUser(c echo.Context) error {
 	name := c.FormValue("name")
 	age := c.FormValue("age")
@@ -32,110 +32,110 @@ func AddUser(c echo.Context) error {
 	if err != nil {
 		return c.String(http.StatusUnauthorized, "Plase upload your profile picture.")
 	}
-	//check email format//
+	//check email format
 	re := regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
-	///email invalid error message in JSON///
+	//email invalid error message in JSON
 	emailError := &ErrorMessage{
 		Code:        "401",
 		Description: "Invalid email format",
 	}
-	///check input data///
+	//check input data
 	if len(name) == 0 {
 		return c.String(http.StatusUnauthorized, "Plase enter your name.")
 	} else if len(age) == 0 {
 		return c.String(http.StatusUnauthorized, "Plase enter your age.")
 	} else if len(email) == 0 {
 		return c.String(http.StatusUnauthorized, "Plase enter your email.")
-		///if email format invavid///
+		//if email format invavid
 	} else if !re.MatchString(email) {
 		return c.JSON(http.StatusUnauthorized, emailError)
 	}
 
-	//souce of image//
+	//souce of image
 	src, err := avatar.Open()
 	if err != nil {
 		return c.JSON(http.StatusInternalServerError, internalError)
 	}
 	defer src.Close()
-	///set path of image in server///
+	//set path of image in server
 	fileName := "img/" + avatar.Filename
 
-	//destination to upload image//
+	//destination to upload image
 	dst, err := os.Create(fileName)
 	if err != nil {
 		return c.JSON(http.StatusUnauthorized, internalError)
 	}
 	defer dst.Close()
 
-	//copy image from souce to destination//
+	//copy image from souce to destination
 	if _, err = io.Copy(dst, src); err != nil {
 		return err
 	}
-	//for get file name to check file type//
+	//for get file name to check file type
 	o, err := os.Open(fileName)
 	if err != nil {
 		return c.JSON(http.StatusUnauthorized, internalError)
 	}
 	defer o.Close()
 
-	//using getFileType//
+	//using getFileType
 	contentType, err := GetFileType(o)
 	if err != nil {
 		return c.JSON(http.StatusUnauthorized, internalError)
 	}
-	///file type error message in JSON///
+	//file type error message in JSON
 	fileError := &ErrorMessage{
 		Code:        "401",
 		Description: "Invalid file type. Upload .png or .jpg/.jpeg only",
 	}
-	///check file type///
+	//check file type
 	if contentType != "image/png" && contentType != "image/jpeg" && contentType != "image/jpg" {
 		os.Remove(fileName)
 		return c.JSON(http.StatusUnauthorized, fileError)
 	}
 
 	conAge, yearOfBirth := CalYearofBirth(age)
-	///age error message in JSON///
+	//age error message in JSON
 	ageError := &ErrorMessage{
 		Code:        "401",
 		Description: "Invalid age. You age must in range of 1 - 100",
 	}
-	///check age validation///
+	//check age validation
 	if conAge <= 0 || conAge > 100 {
 		return c.JSON(http.StatusUnauthorized, ageError)
 	}
-	///add data into AddData function///
-	///check error when email exists///
+	//add data into AddData function
+	//check error when email exists
 	emailExists := &ErrorMessage{
 		Code:        "401",
 		Description: "Email is already used. Plase change email",
 	}
-	///if found count != 0///
+	//if found count != 0
 	if CountEmail(email) > 0 {
 		return c.JSON(http.StatusUnauthorized, emailExists)
 	}
-	///can add data to database///
+	//can add data to database
 	id := AddData(name, avatar.Filename, contentType, conAge, yearOfBirth, note, email)
 	return c.JSON(http.StatusCreated, GetUserData(id))
 }
 
-//GetUser : function get user data from GetUserData to show in HTML//
+//GetUser : function get user data from GetUserData to show in HTML
 func GetUser(c echo.Context) error {
 	id := c.Param("user_id")
-	///check if id param send with invaild format (24-digit)///
+	//check if id param send with invaild format (24-digit)
 	if len(id) != 24 {
 		return c.HTML(http.StatusUnauthorized, "Invalid ID format. Plase try again")
 	}
-	///convert string to bson object///
+	//convert string to bson object
 	bsonID := bson.ObjectIdHex(id)
 
 	u := GetUserData(bsonID)
-	///when cannot found user with this id///
+	//when cannot found user with this id
 	findUserError := &ErrorMessage{
 		Code:        "404",
 		Description: "User not found",
 	}
-	///if not found any data///
+	//if not found any data
 	if u == (UserData{}) {
 		return c.JSON(http.StatusNotFound, findUserError)
 	}
@@ -143,9 +143,9 @@ func GetUser(c echo.Context) error {
 	return c.JSON(http.StatusCreated, u)
 }
 
-//GetAllUser : get data from GetAllUserData to HTML//
+//GetAllUser : get data from GetAllUserData to HTML
 func GetAllUser(c echo.Context) error {
-	///check error, limit value and page value they cannot be 0 or less than///
+	//check error, limit value and page value they cannot be 0 or less than
 	limit, err := strconv.Atoi(c.QueryParam("limit"))
 	if err != nil || limit <= 0 {
 		limit = 10
@@ -154,31 +154,31 @@ func GetAllUser(c echo.Context) error {
 	if err != nil || page <= 0 {
 		page = 1
 	}
-	///store value from GetAllUser in u variable///
+	//store value from GetAllUser in u variable
 	u, err := GetAllUserData(limit, page)
 	if err != nil {
 		return c.JSON(http.StatusUnauthorized, internalError)
 	}
-	///return in JSON format in HTML///
+	//return in JSON format in HTML
 	return c.JSON(http.StatusCreated, u)
 }
 
-//UpdateUser : function for get update user data in database//
+//UpdateUser : function for get update user data in database
 func UpdateUser(c echo.Context) error {
 	id := c.Param("user_id")
-	///check if id param send with invaild format (24-digit)///
+	//check if id param send with invaild format (24-digit)
 	if len(id) != 24 {
 		return c.HTML(http.StatusUnauthorized, "Invalid ID format. Plase try again")
 	}
-	///convert string to bson object///
+	//convert string to bson object
 	bsonID := bson.ObjectIdHex(id)
 
-	///when not found user with this id///
+	//when not found user with this id
 	findUserError := &ErrorMessage{
 		Code:        "404",
 		Description: "User not found",
 	}
-	///if not found any data in database///
+	//if not found any data in database
 	if GetUserData(bsonID) == (UserData{}) {
 		return c.JSON(http.StatusNotFound, findUserError)
 	}
@@ -190,86 +190,86 @@ func UpdateUser(c echo.Context) error {
 	contentType := ""
 	avatarName := ""
 
-	///if user send a new avatar file///
+	//if user send a new avatar file
 	if avatar != nil {
-		//souce of image//
+		//souce of image
 		src, err := avatar.Open()
 		if err != nil {
 			return c.JSON(http.StatusUnauthorized, internalError)
 		}
 		defer src.Close()
-		///set path of image in server///
+		//set path of image in server
 		fileName := "img/" + avatar.Filename
 
-		//destination to upload image//
+		//destination to upload image
 		dst, err := os.Create(fileName)
 		if err != nil {
 			return c.JSON(http.StatusUnauthorized, internalError)
 		}
 		defer dst.Close()
 
-		//copy image from souce to destination//
+		//copy image from souce to destination
 		if _, err = io.Copy(dst, src); err != nil {
 			return c.JSON(http.StatusUnauthorized, internalError)
 		}
-		//for get file name to check file type//
+		//for get file name to check file type
 		o, err := os.Open(fileName)
 		if err != nil {
 			return c.JSON(http.StatusUnauthorized, internalError)
 		}
 		defer o.Close()
 
-		//using getFileType//
+		//using getFileType
 		contentType, err := GetFileType(o)
 		if err != nil {
 			return c.JSON(http.StatusUnauthorized, internalError)
 		}
-		///file type error message in JSON///
+		//file type error message in JSON
 		fileError := &ErrorMessage{
 			Code:        "401",
 			Description: "Invalid file type. Upload .png or .jpg/.jpeg only",
 		}
-		///check file type///
+		//check file type
 		if contentType != "image/png" && contentType != "image/jpeg" && contentType != "image/jpg" {
 			os.Remove(fileName)
 			return c.JSON(http.StatusUnauthorized, fileError)
 		}
-		///set file name///
+		//set file name
 		avatarName = avatar.Filename
 	}
 	conAge, yearOfBirth := 0, 0
 	if age != "" {
 		conAge, yearOfBirth = CalYearofBirth(age)
-		///age error message in JSON///
+		//age error message in JSON
 		ageError := &ErrorMessage{
 			Code:        "401",
 			Description: "Invalid age. You age must in range of 1 - 100",
 		}
-		///check age validation///
+		//check age validation
 		if conAge <= 0 || conAge > 100 {
 			return c.JSON(http.StatusUnauthorized, ageError)
 		}
 	}
-	///using UpdataData to update data in database with this data///
+	//using UpdataData to update data in database with this data
 	userID := UpdateData(bsonID, name, conAge, yearOfBirth, avatarName, note, contentType)
 
 	return c.JSON(http.StatusCreated, GetUserData(userID))
 }
 
-//DeleteUser : function using with DeleteUserData//
+//DeleteUser : function using with DeleteUserData
 func DeleteUser(c echo.Context) error {
 	type status struct {
 		Success bool `json:"success"`
 	}
 	id := c.Param("user_id")
-	///check if id param send with invaild format (24-digit)///
+	//check if id param send with invaild format (24-digit)
 	if len(id) != 24 {
 		return c.HTML(http.StatusUnauthorized, "Invalid ID format. Plase try again")
 	}
-	///convert string to bson object///
+	//convert string to bson object
 	bsonID := bson.ObjectIdHex(id)
 
-	///when not found user with this id///
+	//when not found user with this id
 	findUserError := &ErrorMessage{
 		Code:        "404",
 		Description: "User not found",
--- Rookie/pkg/model.go
+++ Rookie/pkg/model.go
@@ -16,7 +16,7 @@ const (
 	collection = "user_data"
 )
 
-//UserData to handle data//
+//UserData to handle data
 type UserData struct {
 	ID          bson.ObjectId `bson:"_id" json:"_id"`
 	Name        string        `bson:"name" json:"name"`
@@ -30,21 +30,21 @@ type UserData struct {
 	Updatetime  time.Time     `bson:"update_time" json:"update_time"`
 }
 
-//AllUserData using with GetAllUser function//
+//AllUserData using with GetAllUser function
 type AllUserData struct {
 	Count int        `bson:"count" json:"count"`
 	Data  []UserData `bson:"data" json:"data"`
 }
 
-//AddData : add new user data into database//
+//AddData : add new user data into database
 func AddData(name string, avatarName string, avatarType string, age int, yearOfBirth int, note string, email string) bson.ObjectId {
-	///open session to connect database///
+	//open session to connect database
 	session, err := mgo.Dial(server)
 	if err != nil {
 		panic(err)
 	}
 	defer session.Close()
-	///access to database and collection to using data///
+	//access to database and collection to using data
 	a := session.DB(database).C(collection)
 
 	t := time.Now()
@@ -66,37 +66,37 @@ func AddData(name string, avatarName string, avatarType string, age int, yearOfB
 	return add.ID
 }
 
-//GetUserData : function get one user by ID//
+//GetUserData : function get one user by ID
 func GetUserData(id bson.ObjectId) UserData {
-	///open session to connect database///
+	//open session to connect database
 	session, err := mgo.Dial(server)
 	if err != nil {
 		panic(err)
 	}
 	defer session.Close()
-	///access to database and collection to using data///
+	//access to database and collection to using data
 	a := session.DB(database).C(collection)
 	userdata := UserData{}
-	///query user data with ID///
+	//query user data with ID
 	a.Find(bson.M{"_id": id}).One(&userdata)
 	if err != nil {
 		panic(err)
 	}
-	///return in JSON format///
+	//return in JSON format
 	return userdata
 }
 
-//DeleteUserData : function to delete user by id//
+//DeleteUserData : function to delete user by id
 func DeleteUserData(id bson.ObjectId) error {
-	///open session to connect database///
+	//open session to connect database
 	session, err := mgo.Dial(server)
 	if err != nil {
 		return err
 	}
 	defer session.Close()
-	///access to database and collection to using data///
+	//access to database and collection to using data
 	a := session.DB(database).C(collection)
-	///delete data by user id///
+	//delete data by user id
 	err = a.Remove(bson.M{"_id": id})
 	if err != nil {
 		return err
@@ -104,71 +104,71 @@ func DeleteUserData(id bson.ObjectId) error {
 	return err
 }
 
-//GetAllUserData : get all user data from database //
+//GetAllUserData : get all user data from database
 func GetAllUserData(limit, page int) (*AllUserData, error) {
-	///open session to connect database///
+	//open session to connect database
 	session, err := mgo.Dial(server)
 	if err != nil {
 		panic(err)
 	}
 	defer session.Close()
-	///access to database and collection to using data///
+	//access to database and collection to using data
 	a := session.DB(database).C(collection)
-	///variable for store all data of user///
+	//variable for store all data of user
 	usersData := []UserData{}
-	///variable for store data to show with condition limit and page///
+	//variable for store data to show with condition limit and page
 	queryData := []UserData{}
-	///query all of user data///
+	//query all of user data
 	a.Find(nil).Sort("-create_time").All(&usersData)
 	if err != nil {
 		panic(err)
 	}
-	///count all data in database///
+	//count all data in database
 	count, err := a.Find(nil).Count()
-	///start point to query data from condition///
+	//start point to query data from condition
 	startValue := 0
-	///check page///
+	//check page
 	if page > 1 {
-		///start point changed up to page///
+		//start point changed up to page
 		startValue = limit * (page - 1)
 	}
-	///if limit is 1 and page not higher than len of userData (Avoid index out of range)///
+	//if limit is 1 and page not higher than len of userData (Avoid index out of range)
 	if limit == 1 && page <= len(usersData) {
 		queryData = append(queryData, usersData[startValue])
 	} else {
 		for i := startValue; i < startValue+limit; i++ {
-			///avoid a out of range of slices///
+			//avoid a out of range of slices
 			if i >= len(usersData) {
 				break
 			}
-			///query data from userData into queryData///
+			//query data from userData into queryData
 			queryData = append(queryData, usersData[i])
 		}
 	}
-	///store all data to show in show variable///
+	//store all data to show in show variable
 	show := &AllUserData{
 		Count: count,
 		Data:  queryData,
 	}
-	///return in JSON format///
+	//return in JSON format
 	return show, err
 }
 
-//UpdateData : update user data by user id//
+//UpdateData : update user data by user id
 func UpdateData(id bson.ObjectId, name string, age int, yearOfBirth int, avatarName string, note string, avatarType string) bson.ObjectId {
-	///open session to connect database///
+	//open session to connect database
 	session, err := mgo.Dial(server)
 	if err != nil {
 		panic(err)
 	}
 	defer session.Close()
-	///access to database and collection to using data///
+	//access to database and collection to using data
 	a := session.DB(database).C(collection)
-	///calculate year of birth///
+	//calculate year of birth
 	t := time.Now()
 	l, _ := time.LoadLocation("Local")
 
-	///if user change data///
+	//if user change data
 	if name != "" {
 		a.UpdateId(id, bson.M{"$set": bson.M{
 			"name":        name,
@@ -180,11 +180,11 @@ func UpdateData(id bson.ObjectId, name string, age int, yearOfBirth int, avatarN
 			"update_time": t.In(l)}})
 	}
 	if note == "clean" {
-		///if user input in note "clean". note field will be delete///
+		//if user input in note "clean". note field will be delete
 		a.UpdateId(id, bson.M{"$unset": bson.M{"note": ""}})
 		a.UpdateId(id, bson.M{"$set": bson.M{"update_time": t.In(l)}})
 	}
-	///if user send a new avatar file///
+	//if user send a new avatar file
 	if avatarName != "" && avatarType != "" {
 
 		a.UpdateId(id, bson.M{"$set": bson.M{
@@ -201,24 +201,24 @@ func UpdateData(id bson.ObjectId, name string, age int, yearOfBirth int, avatarN
 	return id
 }
 
-//CountEmail function to check email exists in database//
+//CountEmail function to check email exists in database
 func CountEmail(email string) int {
-	///open session to connect database///
+	//open session to connect database
 	session, err := mgo.Dial(server)
 	if err != nil {
 		panic(err)
 	}
 	defer session.Close()
-	///access to database and collection to using data///
+	//access to database and collection to using data
 	a := session.DB(database).C(collection)
-	///check email with count a found data in database///
+	//check email with count a found data in database
 	count, _ := a.Find(bson.M{"email": email}).Count()
 	return count
 }
 
 //GetFileType : function to get file type
 func GetFileType(out *os.File) (string, error) {
-	///read file in first 512 byte to check file type///
+	//read file in first 512 byte to check file type
 	buffer := make([]byte, 512)
 
 	_, err := out.Read(buffer)
@@ -231,12 +231,12 @@ func GetFileType(out *os.File) (string, error) {
 	return contentType, nil
 }
 
-//CalYearofBirth : function to convert age and calculate year of birth//
+//CalYearofBirth : function to convert age and calculate year of birth
 func CalYearofBirth(age string) (int, int) {
-	///calculate year of birth///
+	//calculate year of birth
 	t := time.Now()
 	conAge, _ := strconv.Atoi(age)
-	///calculate year of birth with year now///
+	//calculate year of birth with year now
 	yearOfBirth := t.Year() - conAge
 
 	return conAge, yearOfBirth
