923b0b893c74be035c987c5cb6dbd81cd5cfe0a9
Warhead007 <rawit_ham@hotmail.com>
2019-07-25 11:49:34 +0700

    Add MongoStream strcut

--- twiiter_api/pkg/storestrcut.go
+++ twiiter_api/pkg/storestrcut.go
@@ -3,6 +3,7 @@ package functions
 import (
 	"time"
 
+	"github.com/ChimeraCoder/anaconda"
 	"gopkg.in/mgo.v2/bson"
 )
 
@@ -11,4 +12,17 @@ type FeedData struct {
 	ID       bson.ObjectId `bson:"_id" json:"_id"`
 	Keyword  string        `bson:"keyword" json:"keyword"`
 	FeedTime time.Time     `bson:"feed_time" json:"feed_time"`
+}
+
+//MongoStreams sturct for store data from worker and use in map function
+type MongoStreams struct {
+	ChannelTypeID           string         `bson:"channel_type_id" json:"channel_type_id"`
+	ChannelSouceID          string         `bson:"channel_souce_id" json:"channel_souce_id"`
+	ChannelClassificationID string         `bson:"channel_classification_id" json:"channel_classification_id"`
+	ChannelContentID        string         `bson:"channel_content_id" json:"channel_content_id"`
+	StreamTypeID            string         `bson:"stream_type_id" json:"stream_type_id"`
+	SocialMediaID           string         `bson:"social_media_id" json:"social_media_id"`
+	CreateAt                time.Time      `bson:"create_at" json:"create_at"`
+	UpdateAt                time.Time      `bson:"update_at" json:"update_at"`
+	Payload                 anaconda.Tweet `bson:"payload" json:"payload"`
 }
