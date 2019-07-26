809938fd74b3676672f2e316423815cd80b14063
Warhead007 <rawit_ham@hotmail.com>
2019-07-26 11:42:39 +0700

    Add map function to get data from worker

--- twiiter_api/map/main.go
+++ twiiter_api/map/main.go
@@ -1,9 +1,20 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
 	functions "trainer/twiiter_api/pkg"
 
 	"github.com/streadway/amqp"
+)
+
+const (
+	exchangeNameFromWorker = "workertomap"
+	queueNameFromWorker    = "workertomap"
 )
 
 func main() {
@@ -15,4 +26,26 @@ func main() {
 	cha, err := conn.Channel()
 	functions.FailOnError(err, "Failed to open a channel.")
 	defer cha.Close()
+	//declare queue from master
+	queue, err := functions.DeclareQueue(cha, queueNameFromWorker)
+	functions.FailOnError(err, "Cannot declare queue.")
+	//bind queue to connect queue with exchange from master
+	err = functions.BindQueue(cha, exchangeNameFromWorker, queueNameFromWorker)
+	functions.FailOnError(err, "Cannot binding queue.")
+	//consume data from queue
+	msgs, err := functions.ConsumeData(cha, queue.Name)
+	functions.FailOnError(err, "Consume failed.")
+	fmt.Println("Map starting.")
+	go func() {
+		for d := range msgs {
+			var mongoStreams functions.MongoStreams
+			json.Unmarshal(d.Body, &mongoStreams)
+			fmt.Println(mongoStreams)
+		}
+	}()
+	// Wait for SIGINT and SIGTERM (HIT CTRL-C)
+	ch := make(chan os.Signal)
+	signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
+	log.Println(<-ch)
+	fmt.Println("Map stopped.")
 }
