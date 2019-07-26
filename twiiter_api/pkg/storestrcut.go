e026033f5ecc54874a724a66e5679e72d03a158e
Warhead007 <rawit_ham@hotmail.com>
2019-07-26 21:12:36 +0700

    Add MongoStreams struct

--- twiiter_api/pkg/storestrcut.go
+++ twiiter_api/pkg/storestrcut.go
@@ -20,20 +20,18 @@ type FeedQuery struct {
 	ChannelSouceID          string         `bson:"channel_souce_id" json:"channel_souce_id"`
 	ChannelClassificationID string         `bson:"channel_classification_id" json:"channel_classification_id"`
 	ChannelContentID        string         `bson:"channel_content_id" json:"channel_content_id"`
-	StreamTypeID            string         `bson:"stream_type_id" json:"stream_type_id"`
 	SocialMediaID           string         `bson:"social_media_id" json:"social_media_id"`
 	Payload                 anaconda.Tweet `bson:"payload" json:"payload"`
 }
 
 //MongoStreams sturct for store data from map function to database
 type MongoStreams struct {
-	ChannelTypeID           string         `bson:"channel_type_id" json:"channel_type_id"`
-	ChannelSouceID          string         `bson:"channel_souce_id" json:"channel_souce_id"`
-	ChannelClassificationID string         `bson:"channel_classification_id" json:"channel_classification_id"`
-	ChannelContentID        string         `bson:"channel_content_id" json:"channel_content_id"`
-	StreamTypeID            string         `bson:"stream_type_id" json:"stream_type_id"`
-	SocialMediaID           string         `bson:"social_media_id" json:"social_media_id"`
-	CreateAt                time.Time      `bson:"create_at" json:"create_at"`
-	UpdateAt                time.Time      `bson:"update_at" json:"update_at"`
-	Payload                 anaconda.Tweet `bson:"payload" json:"payload"`
+	ChannelTypeID           string    `bson:"channel_type_id" json:"channel_type_id"`
+	ChannelSouceID          string    `bson:"channel_souce_id" json:"channel_souce_id"`
+	ChannelClassificationID string    `bson:"channel_classification_id" json:"channel_classification_id"`
+	ChannelContentID        string    `bson:"channel_content_id" json:"channel_content_id"`
+	StreamTypeID            string    `bson:"stream_type_id" json:"stream_type_id"`
+	SocialMediaID           string    `bson:"social_media_id" json:"social_media_id"`
+	CreateAt                time.Time `bson:"create_at" json:"create_at"`
+	UpdateAt                time.Time `bson:"update_at" json:"update_at"`
 }
